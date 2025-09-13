a843ddc10cc127e764dab98d5b75ce1bcf7fffae
coffeecode19345 <mebhimthapa@gmail.com>
2025-09-13 07:05:30 +0200

    Update imagemanager.py

--- imagemanager.py
+++ imagemanager.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import unittest
 import sqlite3
 from contextlib import contextmanager
 
